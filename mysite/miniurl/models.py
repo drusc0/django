7eb07e1392479eb4a0ae1c61f8b742770982184c
Andres Rivero <andres.r90@gmail.com>
2020-07-23 22:17:30 -0500

    Added models and admin site for mini url

---
+++ mysite/miniurl/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+STATUS = (
+        (0, "Inactive"),
+        (1, "Active")
+)
+
+# we will create the necessary model for tiny url system
+class Miniurl(models.Model):
+    original_url = models.CharField(max_length=512, unique=True)
+    short_url = models.CharField(max_length=64, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=1)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.original_url} -> {self.short_url}"
+
